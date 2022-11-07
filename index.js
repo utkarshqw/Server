aebe728fcdb8a048aa051b7f73944171cc702947
utkarshqw <utkarshup75@gmail.com>
2022-11-07 20:13:19 +0530

    third commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Info = require("./models/random.schema");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  const data = await Info.find();
+  res.status(200).send(data);
+});
+
+app.listen(5000, async () => {
+  await mongoose.connect(
+    "mongodb+srv://Utkarsh:1234@cluster0.avafjsi.mongodb.net/socketioproject"
+  );
+  console.log("sever started on http://localhost:5000");
+});
